689a6721bbca99d284667aa3a892323a3220801d
GET Academy <terje@kolderup.net>
2021-01-15 11:39:38 +0100

    Initial commit

---
+++ TrafficLight/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TrafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trafficLight = new TrafficLight();
+            Console.WriteLine(trafficLight.IsRedOn);
+            Console.WriteLine(trafficLight.IsYellowOn);
+            Console.WriteLine(trafficLight.IsGreenOn);
+            trafficLight.Next();
+            Console.WriteLine(trafficLight.IsRedOn);
+            Console.WriteLine(trafficLight.IsYellowOn);
+            Console.WriteLine(trafficLight.IsGreenOn);
+        }
+    }
+}
---
+++ TrafficLight/TrafficLight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrafficLight
+{
+    class TrafficLight
+    {
+        private int _phase;
+        public bool IsRedOn => _phase < 2;
+        public bool IsYellowOn => _phase % 2 == 1;
+        public bool IsGreenOn => _phase == 2;
+
+
+        public void Next()
+        {
+            _phase = (_phase + 1) % 4;
+        }
+    }
+}
